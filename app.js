06e39934ebe98b760e6443e492122ed0086ac73f
Josue Igiraneza <igiranezaj28@gmail.com>
2024-09-13 12:20:25 +0200

    feat: Implement get method to get all items

--- app.js
+++ app.js
@@ -3,15 +3,33 @@ const { default: mongoose } = require("mongoose");
 const app = express();
 
 const dbURI =
-   "mongodb+srv://igiranezaj28:Cmnder%2F09@nodejstut.djz3xrv.mongodb.net/nodejs-tut?retryWrites=true&w=majority";
+   "mongodb+srv://igiranezaj28:Cmnder%2F09@nodejstut.djz3xrv.mongodb.net/crudApp?retryWrites=true&w=majority";
 
-const itemSchema = new mongoose.Schema({
-   name: String,
-   description: String,
-   price: Number,
-});
+mongoose
+   .connect(dbURI)
+   .then(() => console.log("Connected to DB"))
+   .catch((err) => console.log(err.message));
+
+const itemSchema = new mongoose.Schema(
+   {
+      name: String,
+      description: String,
+      price: Number,
+   },
+   { timestamps: true }
+);
 
 const Item = mongoose.model("Item", itemSchema);
+
+app.get("/", (req, res) => {
+   Item.find()
+      .sort({ createdAt: -1 })
+      .then((result) => res.status(200).json(result))
+      .catch((err) => {
+         console.log(err);
+         res.status(500).send("Internal server error");
+      });
+});
 
 app.listen(3000, "localhost", () => {
    console.log(" Server Started ============================================");
