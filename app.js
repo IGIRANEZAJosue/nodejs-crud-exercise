dab245c17e190ccf8d3255eebe26570e22129b7b
Josue Igiraneza <igiranezaj28@gmail.com>
2024-09-13 11:46:52 +0200

    create mongoose schema

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
+
+const dbURI =
+   "mongodb+srv://igiranezaj28:Cmnder%2F09@nodejstut.djz3xrv.mongodb.net/nodejs-tut?retryWrites=true&w=majority";
+
+const itemSchema = new mongoose.Schema({
+   name: String,
+   description: String,
+   price: Number,
+});
+
+const Item = mongoose.model("Item", itemSchema);
 
 app.listen(3000, "localhost", () => {
    console.log(" Server Started ============================================");
